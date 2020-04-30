d588328a264a5f227f87105bcaf66b282b6f187f
Jakob Alminde <jakob@alminde.org>
2020-05-01 01:16:04 +0200

    Updates

---
+++ precache-manifest.700ce21e0241bc0ba27d07b6c182b5b6.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97a04d095e37792c8ee3af7cd96e7319",
+    "url": "/namepicker/index.html"
+  },
+  {
+    "revision": "b2512cc8a6d26ba92c26",
+    "url": "/namepicker/static/css/2.095da0fb.chunk.css"
+  },
+  {
+    "revision": "27d5f8935e7b05f220d0",
+    "url": "/namepicker/static/css/main.35b921ed.chunk.css"
+  },
+  {
+    "revision": "b2512cc8a6d26ba92c26",
+    "url": "/namepicker/static/js/2.24168453.chunk.js"
+  },
+  {
+    "revision": "e18637604789af6a40503615de5bd745",
+    "url": "/namepicker/static/js/2.24168453.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27d5f8935e7b05f220d0",
+    "url": "/namepicker/static/js/main.7dec7ed2.chunk.js"
+  },
+  {
+    "revision": "50edf7df9d6c4f28b198",
+    "url": "/namepicker/static/js/runtime-main.140cb9e2.js"
+  },
+  {
+    "revision": "05f1cdadfe476395f60e233b15c22155",
+    "url": "/namepicker/static/media/icons-16.05f1cdad.eot"
+  },
+  {
+    "revision": "3c1c220e7a18286503fb431c7a7fe183",
+    "url": "/namepicker/static/media/icons-16.3c1c220e.woff"
+  },
+  {
+    "revision": "3cde8748332d1de6b1ae1c2dc5850754",
+    "url": "/namepicker/static/media/icons-16.3cde8748.ttf"
+  },
+  {
+    "revision": "0a5c76518a68c185baa2c6744456918c",
+    "url": "/namepicker/static/media/icons-20.0a5c7651.eot"
+  },
+  {
+    "revision": "51ec31f302d0072808e1f83f85fea4cd",
+    "url": "/namepicker/static/media/icons-20.51ec31f3.ttf"
+  },
+  {
+    "revision": "cef8cdbb9d0ba82e6e19fb0eeba2ac3d",
+    "url": "/namepicker/static/media/icons-20.cef8cdbb.woff"
+  }
+]);
